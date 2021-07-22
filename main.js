02c4cd0fcfcbe14735682a1736f6dd2eeb6228b7
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2021-07-22 08:59:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function jij(){
+    window.location = "giveaway.html";
+}
+
+
+
+
+
+function xt(){
+    window.location = "process.html";
+}
+
+
+var freefireid = 0;
+var diamonds = 0;
+var gold = 0;
+
+
+
+
+
+
+
+function xxx(){
+
+
+
+
+
+    
+    freefireid = Number(document.getElementById("ffid").value);
+    gold = Number(document.getElementById("go").value);
+    diamonds = Number(document.getElementById("dia").value);
+
+
+
+
+document.getElementById("iii").innerHTML = "This is your free fire id: " + freefireid;
+document.getElementById("ggg").innerHTML ="You will get " +  gold + " gold.";
+document.getElementById("ddd").innerHTML ="You will get " + diamonds + " diamonds.";
+}
+
+
+
+
+
+function ppp(){
+    window.location = "thankyou.html";
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
